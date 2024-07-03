b09ab1585a956e2a5836260604052b87e0d6f719
Bruno Cocco <Brunoe.cocco@gmail.com>
2024-07-03 14:04:44 +0300

    sumando productos a la tienda

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 console.log("enlazado")
 
+/*Creo Array de objetos */
+const PRODUCTOS = [];
+
+/*Tomando Productos desde la pagina */
+let botonAddItem = document.getElementById("AddItem")
+console.log(botonAddItem)
+
+const MOSTRAR = () => {
+    let nombre = document.getElementsByClassName("itemName")[0].value
+    let precio = document.getElementsByClassName("itemPrice")[0].value
+    let description = document.getElementsByClassName("itemDescription")[0].value
+    PRODUCTOS.push({nombre,precio,description})
+    console.log(PRODUCTOS)
+}
+botonAddItem.addEventListener("click",MOSTRAR)
+/**/
