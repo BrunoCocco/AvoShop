2813d8faf03cb132f368518711db505274063020
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-25 18:59:03 +0100

    agregando componentes principales

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,17 @@
-// import aguacate from "../assets/aguacate.png"
+import aguacate from "../assets/aguacate.png";
 
 function Header() {
   return (
-    <>
-      <header>
-        <article>
-          <h2>AvoShop</h2>
-          {/* <img src={aguacate} alt="aguacate" /> */}
-        </article>
-      </header>
-    </>
+    <header className="header">
+      <article className="header__content">
+        <img
+          src={aguacate}
+          alt="aguacate"
+          className="header__logo"
+        />
+        <h2 className="header__title">AvoShop</h2>
+      </article>
+    </header>
   );
 }
 
