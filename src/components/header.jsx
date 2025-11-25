b8260cafafbd61578ee6f00fca2c01f504db04ab
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-25 18:44:59 +0100

    renombrando cosas

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+// import aguacate from "../assets/aguacate.png"
+
+function Header() {
+  return (
+    <>
+      <header>
+        <article>
+          <h2>AvoShop</h2>
+          {/* <img src={aguacate} alt="aguacate" /> */}
+        </article>
+      </header>
+    </>
+  );
+}
+
+export default Header;
