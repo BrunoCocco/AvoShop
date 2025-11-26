3763d00e6ad55f5662106a96f671ec0fbe468212
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-26 18:36:04 +0100

    solucionando problemas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header.jsx"
+import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
 import { useState } from "react";
